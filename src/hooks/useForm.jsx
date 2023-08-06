3b925e3ef1fafc55b27295bbfafe5ade7a9a8ca7
Lidia Cabrera <ly_judas@hotmail.com>
2023-08-05 23:41:16 -0300

    changes checkout

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,15 +1,17 @@
 import { useReducer } from "react";
+import { validateInput } from "../utils/functions/form-validation";
 
-const INPUTACTIONS = {
+const INPUT_ACTIONS = {
     INPUT_CHANGE: 'INPUT_CHANGE',
     CLEAR_INPUTS: 'CLEAR_INPUTS',
     SET_DATA: 'SET_DATA',
     INPUT_FOCUS: 'INPUT_FOCUS',
+    INPUT_BLUR: 'INPUT_BLUR',
 }
 
 const formReducer = (state, action) => {
     switch (action.type) {
-        case INPUTACTIONS.INPUT_CHANGE:
+        case INPUT_ACTIONS.INPUT_CHANGE:
          const {name, value, error, hasError, isFormValid, active} = action.data;
          return {
             ...state,
@@ -20,17 +22,24 @@ const formReducer = (state, action) => {
                 active,
             },
             isFormValid,
-         }
-         case INPUTACTIONS.INPUT_FOCUS:
-            const {name: inputName, active: inputActive} = action.data;
+         };
+         case INPUT_ACTIONS.INPUT_FOCUS:
             return {
                 ...state,
-                [inputName]: {
-                    ...state[inputName],
-                    active: inputActive,
+                [action.data.name]: {
+                    ...state[action.data.name],
+                    active: true,
                 }
-            }
-        case INPUTACTIONS.CLEAR_INPUTS:
+            };
+        case INPUT_ACTIONS.INPUT_BLUR:
+            return {
+                ...state,
+                [action.data.name]: {
+                    ...state[action.data.name],
+                    active: false,
+                }
+            };
+        case INPUT_ACTIONS.CLEAR_INPUTS:
             return action.data;
         default:
             return state;
@@ -41,33 +50,59 @@ const formReducer = (state, action) => {
 export const useForm = (initialState) => {
     const [formState, dispatchFormState] = useReducer(formReducer, initialState)
 
-    const inputHandler = ({ name, value }) => {
-        dispatchFormState({
-            type: INPUTACTIONS.INPUT_CHANGE,
+    const inputHandler = ({ name, value, dispatch = dispatchFormState }) => {
+        const { error, hasError} = validateInput({ type: name, value});
+        let isFormValid = true;
+
+        for(const key in formState){
+            const item = formState[key];
+            if(key !== name && hasError){
+                isFormValid = false;
+                break;
+            } else if (key !== name && item.hasError) {
+                isFormValid = false;
+                break;
+            }
+        }
+
+        dispatch({
+            type: INPUT_ACTIONS.INPUT_CHANGE,
             data: {
                 name,
                 value,
+                error,
+                hasError,
+                isFormValid,
+                active: true,
             }
         })
     }
     
 
-    const clearInputs = ({ formState }) => {
-        dispatchFormState({
-            type: INPUTACTIONS.CLEAR_INPUTS,
+    const clearInputs = ({ formState, dispatch = dispatchFormState }) => {
+        dispatch({
+            type: INPUT_ACTIONS.CLEAR_INPUTS,
             data: formState,
         })
     }
 
-    const inputFocus = ({ name, active }) => {
-        dispatchFormState({
-            type: INPUTACTIONS.INPUT_FOCUS,
+    const inputFocus = ({ name, dispatch = dispatchFormState }) => {
+        dispatch({
+            type: INPUT_ACTIONS.INPUT_FOCUS,
             data: {
                 name,
-                active,
             }
         })
     }
 
-    return [formState, inputHandler, clearInputs, inputFocus]
+    const inputBlur = ({ name, dispatch = dispatchFormState}) => {
+        dispatch({
+            type: INPUT_ACTIONS.INPUT_BLUR,
+            data: {
+                name,
+            }
+        })
+    }
+
+    return [formState, inputHandler, inputFocus, inputBlur, clearInputs]
 };
